df6edddad7a13ca4905f17b9601590197b7b9ea3
Jonathan Kuo <jonathanpkuo@gmail.com>
2021-08-23 15:12:19 -0700

    Completed function

--- lib/bubble-sort.rb
+++ lib/bubble-sort.rb
@@ -1,5 +1,13 @@
-require 'pry-byebug'
-
 def bubblesort(array)
-
+    length = array.length
+    for i in 0..length
+        for j in 0..(length - i - 2)
+            if array[j] > array[j+1]
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            end
+        end
+    end
+    return array
 end
